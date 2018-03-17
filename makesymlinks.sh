9e4f691fe73e78ca0ace5272766b8eaae69b7fae
Vivian Brown <vivian@eff.org>
2018-03-17 15:09:04 -0700

    Add dir colors to install script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="tmux.conf vimrc bashrc fonts"        # list of files/folders to symlink in homedir
+files="tmux.conf vimrc bashrc fonts dir_colors"        # list of files/folders to symlink in homedir
 
 ##########
 
