303edefd80f112496321ff4ca0a93d926ecc4d26
vivian <vivian@eff.org>
2018-10-10 20:14:34 -0700

    Update i3 and display names for 18.04 compatibility

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if xrandr -q | grep -q 'HDMI2 connected'
+if xrandr -q | grep -q 'HDMI-2 connected'
 then
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.333x1.333 --output HDMI1 --off --output DP2 --off
-elif xrandr -q | grep -q 'HDMI1 connected'
+  xrandr --output HDMI-2 --primary --scale 1.333x1.333 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --mode 2560x1440 --pos 0x1448 --rotate normal --output DP-2 --off
+elif xrandr -q | grep -q 'HDMI-1 connected'
 then
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1600x900 --pos 0x0 --rotate normal --scale 1.33x1.33 --output DP2 --off
+  xrandr --output HDMI-2 --off --output HDMI-1 --primary --scale 1.33x1.33 --mode 1600x900 --pos 176x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 2560x1440 --pos 0x1197 --rotate normal --output DP-2 --off
 else
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+  xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
 fi
