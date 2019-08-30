78e34f419ce3ecf3ffb28fa28e21e66c62b7c3b7
Vivian Brown <vivian.brown@gmail.com>
2019-08-30 15:49:22 -0400

    Add wallpaper

--- screenlayout.sh
+++ screenlayout.sh
@@ -8,3 +8,4 @@ then
 else
   xrandr --output HDMI2 --off --output HDMI1 --off --output DP-1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
 fi
+feh --bg-scale /home/vivian/dotfiles/mountains.jpg
