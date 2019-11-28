bd7a2aa4cb1c3cbb2619607e05469b488a86f031
Vivian Brown <vivian.brown@gmail.com>
2019-11-27 23:37:01 -0800

    Fix display config

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if xrandr -q | grep -q 'HDMI2 connected'
 then
-  xrandr --output HDMI2 --primary --scale 1.333x1.333 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP-1 --off --output eDP1 --mode 2560x1440 --pos 0x1448 --rotate normal --output DP-2 --off
+  xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 224x1620 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.5x1.5 --output HDMI1 --off --output DP2 --off
 elif xrandr -q | grep -q 'HDMI1 connected'
 then
   xrandr --output HDMI2 --off --output HDMI1 --primary --scale 1.33x1.33 --mode 1600x900 --pos 176x0 --rotate normal --output DP-1 --off --output eDP1 --mode 2560x1440 --pos 0x1197 --rotate normal --output DP-2 --off
